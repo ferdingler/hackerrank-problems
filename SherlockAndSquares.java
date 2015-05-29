2d035a32d419bd483d299b5a8e82355974fd57b7
Fer Dingler <ferdingler@gmail.com>
2015-05-28 20:01:11 -0700

    Sherlock and Squares. Best approach

--- SherlockAndSquares.java
+++ SherlockAndSquares.java
@@ -15,25 +15,19 @@ public class SherlockAndSquares {
     	Scanner in = new Scanner(System.in);
     	int t = in.nextInt();
 
-    	int max = 1000000000;
-    	int squareNumbers[] = new int[31624];
-    	int i = 1; 
-    	int n = 0;
-
-    	do {
-    		n = (int) Math.pow(i, 2);
-    		squareNumbers[i++] = n;
-    	} while(n < max);
-
     	for(int x=0; x<t; x++){
             
             int a = in.nextInt();
             int b = in.nextInt();
             int howMany = 0;
+            int n = 0;
+            int i = 1;
 
-            for(int j=0; j<squareNumbers.length; j++){
-            	if(squareNumbers[j] >= a && squareNumbers[j] <= b)
+            while(n <= b){
+            	n = i * i;
+            	if(n >= a && n <= b)
             		howMany++;
+            	i++;
             }
 
             System.out.println(howMany);
