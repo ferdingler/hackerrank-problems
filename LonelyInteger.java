eef56af8b02dc3d3d0c9369e1d5981149e5b4431
Dingler Avilez, Fernando <Fernando.DinglerAvilez@lifetech.com>
2017-02-14 21:41:47 -0800

    LonelyInteger using lambda functions

--- LonelyInteger.java
+++ LonelyInteger.java
@@ -10,12 +10,8 @@ import java.util.regex.*;
 
 public class LonelyInteger {
 
-    private static int lonelyInteger(int[] a) {
-        int result = 0;
-        for (int n : a) {
-            result ^= n;
-        }
-        return result;
+    private static int lonelyInteger(int[] arr) {
+        return Arrays.stream(arr).reduce(0, (int a, int b) -> a ^ b);
     }
 
     public static void main(String[] args) {
