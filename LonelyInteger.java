edeab69cd901b298fb6719360f62ca1f3705ce43
Dingler Avilez, Fernando <Fernando.DinglerAvilez@lifetech.com>
2017-02-14 21:33:23 -0800

    Slight better version of LonelyInteger - HashMap

--- LonelyInteger.java
+++ LonelyInteger.java
@@ -4,44 +4,37 @@ import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-/*
-Problem URL: https://www.hackerrank.com/challenges/lonely-integer
-*/
+/**
+ * https://www.hackerrank.com/challenges/ctci-lonely-integer
+ */
 
 public class LonelyInteger {
-   
-    static int lonelyinteger(int[] a) {
-        
-        int[] numbers = new int[101];
-        for(int i=0; i<a.length; i++){
-            numbers[a[i]] += 1;
+
+    private static int lonelyInteger(int[] a) {
+
+        int[] counts = new int[100];
+        for (int n : a) {
+            counts[n] += 1;
         }
-        
-        for(int i=0; i<numbers.length; i++){
-            if(numbers[i] == 1)
+
+        for(int i=0; i<counts.length; i++){
+            if(counts[i] == 1){
                 return i;
+            }
         }
-        
-        return 0;
+
+        return a[0];
     }
-   
-    public static void main(String[] args) {    
-       Scanner in = new Scanner(System.in);
-       int res;
-        
-       int _a_size = Integer.parseInt(in.nextLine());
-       int[] _a = new int[_a_size];
-       int _a_item;
-       String next = in.nextLine();
-       String[] next_split = next.split(" ");
-        
-       for(int _a_i = 0; _a_i < _a_size; _a_i++) {
-           _a_item = Integer.parseInt(next_split[_a_i]);
-           _a[_a_i] = _a_item;
-       }
-        
-       res = lonelyinteger(_a);
-       System.out.println(res);
-        
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a[] = new int[n];
+        for(int i=0; i<n; i++){
+            a[i] = in.nextInt();
+        }
+
+        System.out.println(lonelyInteger(a));
     }
 }
